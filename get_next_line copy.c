25a349e8dfd8db5809ddd203857008aba8f57e60
Mai Funyu <mfunyu@student.42tokyo.jp>
2020-07-01 19:16:22 +0900

    fornow

--- get_next_line copy.c
+++ get_next_line copy.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 18:32:49 by mfunyu            #+#    #+#             */
-/*   Updated: 2020/07/01 18:45:17 by mfunyu           ###   ########.fr       */
+/*   Updated: 2020/07/01 15:42:45 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,64 +20,53 @@ int		get_next_line(int fd, char **line)
 	static char	*save;
 	int		index;
 	int		i;
-	char	*newline;
+	char	*newlineptr;
 	// static int		sum;
 	int		bytes;
 
 	i = 0;
 	index = 0;
-
-	printf("save: \"%s\"\n", save);
-	bytes = read(fd, buf, BUFFER_SIZE);
-	if (bytes == -1)
+	// buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	// if (!read(fd, buf, BUFFER_SIZE))
+	// 	return (-1);
+	
+	// read(fd, *line, 1);
+	while ((bytes = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		if ((newlineptr = ft_strchr(buf, '\n')))
+		{
+			index = newlineptr - buf + 1;
+			ft_strlcat(*line, buf, index);
+			*line[index] = '\0';
+			save = (char *)malloc((BUFFER_SIZE + 1) + sizeof(char));
+			if (index > BUFFER_SIZE)
+			{
+				ft_strlcpy(save, buf + index, BUFFER_SIZE + 1);
+			}
+			return (1);
+		}
+		else
+		{
+			ft_strlcat(*line, buf, 100);
+		}
+	}
+	if (bytes == 0)
+	{
+		ft_strlcat(*line, buf, 100);
+		return (0);
+	}
+	else if (bytes == -1)
 	{
 		return (-1);
 	}
-	printf("yes\n");
-	if (bytes == 0)
-	{
-		printf("yes\n");
-		return (0);
-		if ((newline = ft_strchr(buf, '\n')))
-		{
-			index = newline - buf + 1;
-			ft_strlcat(*line, buf, index);
-			if (index < BUFFER_SIZE)
-			{
-				save = (char *)malloc((BUFFER_SIZE - index + 1) + sizeof(char));
-				ft_strlcpy(save, buf + index, BUFFER_SIZE + 1);
-			}
-			else
-			{
-				ft_strlcat(*line, buf, 100);
-			}
-			return (1);
-		}
-		ft_strlcat(*line, buf, 100);
-		return (0);
-	}
-	else
-	{
-		printf("yes\n");
-		printf("buf: \"%s\"\n", buf);
-		if ((newline = ft_strchr(buf, '\n')))
-		{
-			printf("yes\n");
-			index = newline - buf + 1;
-			ft_strlcat(*line, buf, index);
-			if (index < BUFFER_SIZE)
-			{
-				save = (char *)malloc((BUFFER_SIZE - index + 1) + sizeof(char));
-				ft_strlcpy(save, buf + index, BUFFER_SIZE + 1);
-			}
-			else
-			{
-				ft_strlcat(*line, buf, 100);
-			}
-			return (1);
-		}
-		ft_strlcat(*line, buf, 100);
-		}
+	// sum += index;
+	// printf("i: %d\n", index);
+	// printf("bytes: %d\n", bytes);
+	// printf("sum: %d\n", sum);
+	// printf("line: %s\n", *line);
+	// printf("buf: %s\n", buf);
+	// ft_strlcat(*line, buf, 100);
+	// free(save);
 	return (1);
 }
 
