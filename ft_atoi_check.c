95b7989ab79c2350c5a362f03c7222390e699426
mfunyu <mfunyu@student.42tokyo.jp>
2021-05-18 20:52:53 +0900

    [fix] error cases

--- ft_atoi_check.c
+++ ft_atoi_check.c
@@ -6,11 +6,18 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 13:03:35 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/04/09 21:13:42 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/05/18 18:52:32 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	ft_skip_spaces(const char **str)
+{
+	while (**str == ' ' || **str == '\n' || **str == '\t'
+		|| **str == '\v' || **str == '\f' || **str == '\r')
+		(*str)++;
+}
 
 int	ft_atoi_check(const char *n, int *error)
 {
@@ -20,22 +27,20 @@ int	ft_atoi_check(const char *n, int *error)
 	nb = 0;
 	sign = 1;
 	*error = ERROR;
-	while (*n == ' ' || *n == '\n' || *n == '\t'
-		|| *n == '\v' || *n == '\f' || *n == '\r')
-		n++;
+	ft_skip_spaces(&n);
 	if (*n == '-' || *n == '+')
-	{
-		if (*n == '-')
-			sign *= -1;
-		n++;
-	}
+		sign *= 44 - *n++;
 	while (ft_isdigit(*n))
 	{
+		*error = 1;
 		nb = nb * 10 + (*n++ - '0');
 		if (nb * sign < INT_MIN || INT_MAX < nb * sign)
+		{
+			*error = ERROR;
 			return (0);
+		}
 	}
-	if (*n == '\0')
+	if (*error == 1 && *n == '\0')
 		*error = SUCCESS;
 	return ((int)(nb * sign));
 }
