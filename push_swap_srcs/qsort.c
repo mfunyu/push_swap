85056e43db8716ffa2047e52abd77f67bfbd5f3b
mfunyu <mfunyu@student.42tokyo.jp>
2021-05-20 17:02:12 +0900

    [fix] qsort dup

--- push_swap_srcs/qsort.c
+++ push_swap_srcs/qsort.c
@@ -9,29 +9,50 @@ static void	swap_for_qsort(t_stack *x, t_stack *y)
 	*x = tmp;
 }
 
-void	qsort_stack(t_stack sort_stack[], int first, int last)
+int	get_pivot(t_stack sort_stack[], int first, int last)
+{
+	while (first < last)
+	{
+		if (sort_stack[first].elem != sort_stack[first + 1].elem)
+			return (sort_stack[first].elem);
+		first++;
+	}
+	return (-1);
+}
+
+int	partition(t_stack sort_stack[], int first, int last)
 {
 	int		i;
 	int		j;
 	int		pivot;
 
-	if (first < last)
+	i = first;
+	j = last;
+	pivot = get_pivot(sort_stack, first, last);
+	if (pivot == -1)
+		return (pivot);
+	while (i < j)
 	{
-		i = first;
-		j = last;
-		pivot = first;
-		while (i < j)
-		{
-			while (sort_stack[i].elem <= sort_stack[pivot].elem)
-				i++;
-			while (sort_stack[pivot].elem < sort_stack[j].elem)
-				j--;
-			if (i < j)
-				swap_for_qsort(&sort_stack[i], &sort_stack[j]);
-		}
-		swap_for_qsort(&sort_stack[pivot], &sort_stack[j]);
-		pivot = j;
-		qsort_stack(sort_stack, first, pivot - 1);
-		qsort_stack(sort_stack, pivot + 1, last);
+		while (i < last && sort_stack[i].elem < pivot)
+			i++;
+		while (first < j && pivot < sort_stack[j].elem)
+			j--;
+		if (i > j)
+			break ;
+		swap_for_qsort(&sort_stack[i], &sort_stack[j]);
 	}
+	return (i);
+}
+
+void	qsort_stack(t_stack sort_stack[], int first, int last)
+{
+	int		pivot;
+
+	if (first >= last)
+		return ;
+	pivot = partition(sort_stack, first, last);
+	if (pivot == -1)
+		return ;
+	qsort_stack(sort_stack, first, pivot - 1);
+	qsort_stack(sort_stack, pivot + 1, last);
 }
