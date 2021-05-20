315756ed9918a4d2f5ada130d07c90969eaf621e
mfunyu <mfunyu@student.42tokyo.jp>
2021-05-20 17:02:47 +0900

    [fxi] 3 random sort

--- push_swap_srcs/sort_three.c
+++ push_swap_srcs/sort_three.c
@@ -1,23 +1,21 @@
 #include "push_swap.h"
 
-void	sort_three(t_stack **stack_a, t_list **instructions)
+void	sort_three(t_stack **stack_a, t_list **instructions, t_stack_type type)
 {
-	int		first;
-	int		middle;
-	int		last;
+	t_stack	*first;
+	t_stack	*second;
 
-	first = (*stack_a)->elem;
-	middle = (*stack_a)->next->elem;
-	last = (*stack_a)->next->next->elem;
-	if (first < middle && middle < last)
+	first = *stack_a;
+	second = (*stack_a)->next;
+	if (first->order == 0 && second->order == 1)
 		return ;
-	else if (last < first && first < middle)
-		exec_add_instructions(stack_a, NULL, instructions, rra);
-	else if (middle < last && last < first)
-		exec_add_instructions(stack_a, NULL, instructions, ra);
+	else if (first->order == 1 && second->order == 2)
+		exec_add_instructions(stack_a, NULL, instructions, rra + type);
+	else if (first->order == 2 && second->order == 0)
+		exec_add_instructions(stack_a, NULL, instructions, ra + type);
 	else
 	{
-		exec_add_instructions(stack_a, NULL, instructions, sa);
-		sort_three(stack_a, instructions);
+		exec_add_instructions(stack_a, NULL, instructions, sa + type);
+		sort_three(stack_a, instructions, type);
 	}
 }
